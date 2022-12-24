9f649a135d9731081755f22eaef0e64fd356ba5c
Simen Sandhaug <simen.sandhaug@gmail.com>
2022-12-24 16:54:29 +0100

    fixed azure search function endpoint in javascript

--- nlp-function-api/search/index.js
+++ nlp-function-api/search/index.js
@@ -1,23 +1,21 @@
 // import openai embedding
-const { OpenAI } = require('openai-api');
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
+// const { OpenAI } = require("openai-api");
+//const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
-
-const get_embedding = async (query) => {
-    // POST https://api.openai.com/v1/embeddings
-    const response = await fetch('https://api.openai.com/v1/embeddings', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-        },
-        body: JSON.stringify({
-            "model": "text-embedding-ada-002",
-            "input": query
-        }
-        )
-    });
-    /* response format: {
+/* const get_embedding = async (query) => {
+  // POST https://api.openai.com/v1/embeddings
+  const response = await fetch("https://api.openai.com/v1/embeddings", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "text-embedding-ada-002",
+      input: query,
+    }),
+  });
+  /* response format: {
   "object": "list",
   "data": [
     {
@@ -36,38 +34,51 @@ const get_embedding = async (query) => {
     "prompt_tokens": 8,
     "total_tokens": 8
   }
-}*/
-    const data = await response.json();
-    return data.data[0].embedding;
 }
-
-
-
+  const data = await response.json();
+  return data.data[0].embedding;
+}; 
+*/
+const axios = require("axios");
+const apiKey = "nmiv4f6MgA78R47rstmgbWTYstV3nix2";
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-    const prompt = (req.body && req.body.name);
+  const prompt = req.body && req.body.prompt;
 
-    const embedding = await get_embedding(prompt);
+  const data = { prompt: "Hvor mye skatt må jeg betale?" };
 
-    // connect to the azure ml blue endpoint
-    const response = await fetch('https://lovdata-endpoint-1.northeurope.inference.ml.azure.com/score', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            //Authorization: Bearer $ENDPOINT_CRED
-            "Authorization": `Bearer ${process.env.ENDPOINT_CRED}
-        },
-        body: JSON.stringify({
-            "prompt": embedding
-        }
-        )
-    });
+  const body = JSON.stringify(data);
 
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${apiKey}`,
+    "azureml-model-deployment": "blue",
+  };
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+  const url =
+    "https://lovdata-endpoint-1.northeurope.inference.ml.azure.com/score";
+  /* const embedding = await get_embedding(prompt); */
+
+  // connect to the azure ml blue endpoint
+
+  /*   const response = await fetch(url, {
+    method: "POST",
+    headers: headers,
+    body: body,
+  }); */
+
+  const response = await axios.post(url, body, {
+    headers: headers,
+  });
+
+  const res = {
+    // status: 200, /* Defaults to 200 */
+    body: JSON.parse(response.data, { encoding: "utf8" }),
+  };
+
+  context.log(res);
+
+  context.res = res;
+};
