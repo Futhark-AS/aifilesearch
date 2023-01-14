0d9c2e320c872a167fc9eb23932a281c666b2b00
Jørgen Sandhaug <jorgen.sandhaug@gmail.com>
2023-01-14 10:56:00 +0100

    add endpoint for getFiles

---
+++ backend/nlp-search-api/getFiles/index.js
@@ -0,0 +1,25 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const document = context.bindings.document;
+    const project = req.params.project;
+
+    if (!document) {
+        context.res = {
+            status: 400,
+            body: "No projects found for user"
+        };
+        return;
+    }
+    let body = {};
+    document.projects.forEach((p) => {
+        if (p.namespace.split("/")[1] === project) {
+            body = p.files;
+        }
+    }); 
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body
+    };
+}
