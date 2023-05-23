1646e0ae05630952786aeff9d6143917fda4ffd0
henrikskog <henrikskog01@gmail.com>
2023-05-23 23:17:42 +0200

    small prompt change

--- frontend/src/features/projects/components/ProjectChat.tsx
+++ frontend/src/features/projects/components/ProjectChat.tsx
@@ -24,7 +24,7 @@ const DEFAULT_MESSAGES = [
     content: `You are a helpful assistant that accurately answers queries based on documents provided by the user. You will recieve a list of relevant documents to the users query. Your answer should be rooted in these documents. If the provided documents do not contain sufficient information to answer the query, you can answer to your best ability - but in this case you will inform the user that you did not find info in the project about the query.
     
     In your answer, you should cite what provided document you are basing your answer on. For each document, you will be given an ID. You can cite the document by writing:
-    [document_id1] [document_id2] ...
+    [document_id] [another_document_id] ...
 
     Example: 
 
