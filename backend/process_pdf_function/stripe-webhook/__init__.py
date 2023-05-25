1f40f8412e2b0e66662dc912c3545d6b430c2c97
henrikskog <henrikskog01@gmail.com>
2023-05-25 11:47:22 +0200

    notify

--- backend/process_pdf_function/stripe-webhook/__init__.py
+++ backend/process_pdf_function/stripe-webhook/__init__.py
@@ -2,20 +2,30 @@ import logging
 import stripe
 from azure.cosmos import CosmosClient
 import os
+import requests
 
 import azure.functions as func
 
-#----------- COsmos DB ----------
+# ----------- COsmos DB ----------
 cosmos_endpoint = "https://nlpcosmos.documents.azure.com:443/"
 cosmos_key = os.getenv("ENV_COSMOS_KEY")
 cosmos_client = CosmosClient(url=cosmos_endpoint, credential=cosmos_key)
 logging.info("Cosmos DB client created")
-cosmos_database = cosmos_client.get_database_client("nlp-search") 
+cosmos_database = cosmos_client.get_database_client("nlp-search")
 cosmos_container = cosmos_database.get_container_client("users")
 
-#----------- Stripe ----------
+# ----------- Stripe ----------
 stripe.api_key = os.getenv("ENV_STRIPE_SECRET_KEY")
 WEBHOOK_SECRET = os.getenv("ENV_STRIPE_WEBHOOK_SECRET")
+
+def notify_of_event(messages):
+    url = os.getenv("ENV_NOTIFY_URL_ENDPOINT")
+    dest = os.getenv("ENV_NOTIFIER_DESTINATION")
+    if not url or not dest:
+        return
+
+    requests.post(url, json={"to": dest, "messages": messages})
+
 
 def update_user_credits(user_id, credits_to_buy):
     user = cosmos_container.read_item(item=user_id, partition_key=user_id)
@@ -27,15 +37,14 @@ def update_user_credits(user_id, credits_to_buy):
 
     cosmos_container.upsert_item(user)
     logging.info(f"User now has {user['credits']} credits, after buying {credits_to_buy} credits")
+    notify_of_event([f"User {user['email']} bought {credits_to_buy}"])
+
 
 def handle_webhook(payload, sig, endpoint_secret):
     event = None
 
-
     try:
-        event = stripe.Webhook.construct_event(
-            payload, sig, endpoint_secret
-        )
+        event = stripe.Webhook.construct_event(payload, sig, endpoint_secret)
     except ValueError as e:
         logging.info("Invalid payload {}".format(e))
         # Invalid payload
@@ -46,28 +55,28 @@ def handle_webhook(payload, sig, endpoint_secret):
         raise e
 
     # Handle the event
-    if event['type'] == 'payment_intent.succeeded':
-        payment_intent = event['data']['object']
+    if event["type"] == "payment_intent.succeeded":
+        payment_intent = event["data"]["object"]
 
-        uid = payment_intent['metadata']['uid']
-        credits = int(payment_intent['metadata']['credits'])
+        uid = payment_intent["metadata"]["uid"]
+        credits = int(payment_intent["metadata"]["credits"])
         update_user_credits(uid, credits)
 
     else:
-      logging.info('Unhandled event type {}'.format(event['type']))
+        logging.info("Unhandled event type {}".format(event["type"]))
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
     # loop over all the headers and print them
     logging.info(req.headers.__dict__)
-    
+
     logging.info(req.get_json())
 
     try:
         req_body = req.get_body()
-        sig = req.headers.get('stripe-signature')
+        sig = req.headers.get("stripe-signature")
 
         logging.info("sig is " + sig)
 
@@ -78,7 +87,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     except Exception as e:
         logging.info("Error in webhook", exc_info=True)
 
-        return func.HttpResponse(
-                "Error in webhook " + str(e),
-                status_code=400
-            )
+        return func.HttpResponse("Error in webhook " + str(e), status_code=400)
