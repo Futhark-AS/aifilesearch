36ea5235928a3d655c69fb1f2ef5bbc4e795aa98
henrikskog <henrikskog01@gmail.com>
2023-05-25 11:37:57 +0200

    improve chat prompt

--- frontend/src/features/projects/components/ProjectChat.tsx
+++ frontend/src/features/projects/components/ProjectChat.tsx
@@ -132,22 +132,30 @@ const DEFAULT_MESSAGES = [
     content: `You are a helpful assistant that accurately answers queries based on documents provided by the user. You will recieve a list of relevant documents to the users query. Your answer should be rooted in these documents. If the provided documents do not contain sufficient information to answer the query, you can answer to your best ability - but in this case you will inform the user that you did not find info in the project about the query.
     
     In your answer, you should cite what provided document you are basing your answer on. For each document, you will be given an ID. You can cite the document by writing:
-    [document_id] [another_document_id] ...
+    [doc_id_1] [doc_id_2] ...
+    You will NOT citate using this format: [doc_id_1, doc_id_2, ...]
 
     Example: 
-
     documents: [{
       id: 1,
-      text: "Paris is the capital of France"
+      text: "In 2020, there was 100 rain days in Oslo, Norway"
     }, {
       id: 2,
-      text: "Norway is a country in Europe"
-    }]
-    query: What is the capital of France?
+      text: "In 2018, there was 200 rain days in Oslo, Norway"
+    },
+    {
+      id: 3,
+      text: "In 2019, there was 150 rain days in Oslo, Norway"
+    }
+  ]
 
+    query: "From 2018 to 2020, how many rain days were there in Oslo, Norway?"
 
-    Your answer:
-    Paris is the capital of France [1]
+    Your answer alternative 1:
+    "There was 200 rain days in 2018 [2], 150 rain days in 2019 [3] and 100 rain days in 2020 [0]. Thus, there was 450 rain days in total."
+
+    Your answer alternative 2:
+    "There was 450 rain days in total [0] [2] [3]."
     `,
   },
   {
