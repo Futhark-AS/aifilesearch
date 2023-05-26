cab205d4fa46620e995c407e5b2aec1e376fdb90
henrikskog <henrikskog01@gmail.com>
2023-05-26 18:06:04 +0200

    bad urls

--- frontend/src/features/users/components/StripePayment.tsx
+++ frontend/src/features/users/components/StripePayment.tsx
@@ -7,9 +7,9 @@ import { StripeCheckoutForm } from "@/components/StripeCheckoutForm";
 export const successURL = (credits: number) =>
   import.meta.env.VITE_PROD == "1"
     ? import.meta.env.VITE_PROD_URL +
-      `payment-success?credits=${credits}`
+      `/payment-success?credits=${credits}`
     : import.meta.env.VITE_DEV_URL +
-      `payment-success?credits=${credits}`;
+      `/payment-success?credits=${credits}`;
 
 // custom trigger button
 interface Props {
