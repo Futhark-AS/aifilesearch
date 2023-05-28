c9a99976903ff275a856773a2e212dd4182d5548
henrikskog <henrikskog01@gmail.com>
2023-05-28 17:04:42 +0200

    remove highlighted box when navigating away from pdf

--- frontend/src/routes/protected.tsx
+++ frontend/src/routes/protected.tsx
@@ -2,12 +2,24 @@ import { MainLayout } from "@/components/Layout";
 import { useAuth } from "@/features/auth/hooks/useAuth";
 import { Dashboard } from "@/features/misc";
 import { ProjectRoutes } from "@/features/projects/routes";
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { UserRoutes } from "../features/users/routes/index";
+import { useAppDispatch } from "@/redux/hooks";
+import { setHighlightedResult } from "@/features/projects/projectSlice";
 
 const App = () => {
   useAuth({ refetch: true });
+
+
+  // Basically a hack to reset highlighted result when user navigates away from pdf page.
+  const dispatch = useAppDispatch();
+  const { pathname } = useLocation();
+  useEffect(() => {
+    if (!pathname.includes("/pdf")) {
+      dispatch(setHighlightedResult(null));
+    }
+  }, [pathname, dispatch]);
 
   return (
     <MainLayout>
